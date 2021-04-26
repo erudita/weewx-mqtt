bed62ca7b06f6f9e915d6ec32f2ab40fd9beb929
erudita <49094080+erudita@users.noreply.github.com>
2021-04-26 18:26:56 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,13 @@ HOMEDIR=/home/${WEEWX_UID:-weewx}
 PATH=$HOMEDIR/bin:$PATH
 
 # edit initial distribution
-if [ ! -e $HOMEDIR/weewx.conf.bak ]; then
+if [ ! -e $HOMEDIR/weewx.conf.orig ]; then
   echo "INITAL SETTINGS"
+  cp $HOMEDIR/weewx.conf $HOMEDIR/weewx.conf.orig
+  
+# add interceptor driver details  
+  if [ -f $HOMEDIR/interceptor.conf ]; then
+    cat $HOMEDIR/interceptor.conf >> $HOMEDIR/weewx.conf
 fi
 
 ./bin/weewxd
