90779d7210c840c3fbaccad257c86a93df9c49f0
erudita <49094080+erudita@users.noreply.github.com>
2021-04-26 23:53:26 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -e
 
-# set timezone
-if [ ! -f /etc/timezone ] && [ ! -z "$TZ" ]; then
-  # At first startup, set timezone
-  cp /usr/share/zoneinfo/$TZ /etc/localtime
-  echo $TZ >/etc/timezone
-fi
 
 # home directory
 HOMEDIR=/home/${WEEWX_UID:-weewx}
